675117b5fd70ccabb913737a15b151e5b3bf759c
romaisa1122 <l242608@lhr.nu.edu.pk>
2026-05-10 21:15:31 +0500

    Update leaveModel.js

--- backend/Models/leaveModel.js
+++ backend/Models/leaveModel.js
@@ -1,37 +1,59 @@
 const { sql, poolPromise } = require('../config/db');
 
 const Leave = {
-  async getAllLeaves() {
+
+  getAllLeaves: async () => {
     const pool = await poolPromise;
+
     const result = await pool.request().query(`
       SELECT l.leave_id, s.name AS student_name,
              l.from_date, l.to_date, l.status
       FROM LeaveRequests l
       JOIN Students s ON l.student_id = s.student_id
     `);
+
     return result.recordset;
   },
 
-  async requestLeave(student_id, from_date, to_date) {
+  getLeavesByStudent: async (studentId) => {
     const pool = await poolPromise;
-    await pool.request()
-      .input('student_id', sql.Int, student_id)
-      .input('from_date', sql.Date, from_date)
-      .input('to_date', sql.Date, to_date)
-      .query(`
-        INSERT INTO LeaveRequests (leave_id, student_id, from_date, to_date, status)
-        VALUES ((SELECT ISNULL(MAX(leave_id),0)+1 FROM LeaveRequests),
-                @student_id, @from_date, @to_date, 'Pending')
-      `);
-  },
 
-  async updateLeaveStatus(id, status) {
+    const result = await pool.request()
+      .input('studentId', sql.Int, studentId)
+      .query(`
+        SELECT leave_id, from_date, to_date, status
+        FROM LeaveRequests
+        WHERE student_id = @studentId
+      `);
+
+    return result.recordset;
+  },
+requestLeave: async (student_id, from_date, to_date) => {
+  const pool = await poolPromise;
+
+  await pool.request()
+    .input('student_id', sql.Int, student_id)
+    .input('from_date', sql.Date, from_date)
+    .input('to_date', sql.Date, to_date)
+    .query(`
+      INSERT INTO LeaveRequests (student_id, from_date, to_date, status)
+      VALUES (@student_id, @from_date, @to_date, 'Pending')
+    `);
+},
+
+  updateLeaveStatus: async (id, status) => {
     const pool = await poolPromise;
+
     await pool.request()
       .input('id', sql.Int, id)
       .input('status', sql.VarChar, status)
-      .query('UPDATE LeaveRequests SET status=@status WHERE leave_id=@id');
+      .query(`
+        UPDATE LeaveRequests
+        SET status = @status
+        WHERE leave_id = @id
+      `);
   }
+
 };
 
 module.exports = Leave;
