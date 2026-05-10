aaefcc369aeb7cb9c353fbac1ce5c5d18d2cfdc5
romaisa1122 <l242608@lhr.nu.edu.pk>
2026-05-10 21:16:47 +0500

    Update complaintModel.js

--- backend/Models/complaintModel.js
+++ backend/Models/complaintModel.js
@@ -5,7 +5,7 @@ const Complaint = {
     const pool = await poolPromise;
     const result = await pool.request().query(`
       SELECT c.complaint_id, s.name AS student_name,
-             c.description, cs.status_name, c.date_submitted
+             c.category, c.description, cs.status_name, c.date_submitted
       FROM Complaints c
       JOIN Students s ON c.student_id = s.student_id
       JOIN ComplaintStatus cs ON c.status_id = cs.status_id
@@ -20,16 +20,30 @@ const Complaint = {
     return result.recordset;
   },
 
-  async addComplaint(student_id, description) {
+  async getByStudent(id) {
+    const pool = await poolPromise;
+    const result = await pool.request()
+      .input('id', sql.Int, id)
+      .query(`
+        SELECT c.complaint_id, c.category, c.description, c.date_submitted,
+               cs.status_name
+        FROM Complaints c
+        JOIN ComplaintStatus cs ON c.status_id = cs.status_id
+        WHERE c.student_id = @id
+      `);
+    return result.recordset;
+  },
+
+  async addComplaint(student_id, category, description) {
     const pool = await poolPromise;
     await pool.request()
       .input('student_id', sql.Int, student_id)
-      .input('description', sql.VarChar, description)
-      .query(`
-        INSERT INTO Complaints (complaint_id, student_id, description, status_id, date_submitted)
-        VALUES ((SELECT ISNULL(MAX(complaint_id),0)+1 FROM Complaints),
-                @student_id, @description, 1, CAST(GETDATE() AS DATE))
-      `);
+      .input('category', sql.VarChar, category)
+      .input('description', sql.VarChar, description)  
+  .query(`
+  INSERT INTO Complaints (student_id, description, status_id, date_submitted, category)
+  VALUES (@student_id, @description, 1, CAST(GETDATE() AS DATE), @category)
+`);
   },
 
   async resolveComplaint(id) {
